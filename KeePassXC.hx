b6324220d80248440e5d7376cceffe383f4e0ee4
Josu Igoa <josu.igoa@indaba.lks.es>
2023-03-14 23:38:37 +0100

    first commit

---
+++ KeePassXC.hx
@@ -0,0 +1,201 @@
+package;
+
+using api.IdeckiaApi;
+using StringTools;
+
+typedef Props = {
+	@:editable("The path to the database")
+	var database_path:String;
+	@:editable("Show entries from this folder (empty shows all entries)", "")
+	var database_root_folder:String;
+	@:editable("Name of the parent directory", {
+		toDir: "_main_",
+		text: "back",
+		textSize: null,
+		textColor: null,
+		textPosition: null,
+		icon: "folder",
+		bgColor: "ffff0000"
+	})
+	var parent_dir_state:{
+		toDir:String,
+		text:String,
+		textSize:UInt,
+		textColor:String,
+		textPosition:TextPosition,
+		icon:String,
+		bgColor:String
+	};
+}
+
+@:name("keepassxc")
+@:description("Load all the password saved in the keepassxc database and creates an item for each")
+class KeePassXC extends IdeckiaAction {
+	var databasePassword:String = '';
+	var dynamicDir:DynamicDir;
+
+	override function init(initialState:ItemState):js.lib.Promise<ItemState> {
+		loadEntriesFromCSV().catchError(e -> server.log.error(e));
+		return super.init(initialState);
+	}
+
+	public function execute(currentState:ItemState):js.lib.Promise<ActionOutcome> {
+		return new js.lib.Promise((resolve, reject) -> {
+			loadEntriesFromCSV().then(_ -> resolve(new ActionOutcome({directory: dynamicDir}))).catchError(e -> server.log.error(e));
+		});
+	}
+
+	override public function onLongPress(currentState:ItemState):js.lib.Promise<ActionOutcome> {
+		databasePassword = '';
+		dynamicDir = null;
+		return execute(currentState);
+	}
+
+	function getKeePassXCPassword() {
+		return new js.lib.Promise((resolve, reject) -> {
+			if (databasePassword != '') {
+				resolve(databasePassword);
+				return;
+			}
+
+			server.dialog.password('KeePassXC [${haxe.io.Path.withoutDirectory(props.database_path)}] file password',
+				'Write the KeePassXC [${props.database_path}] file password, please')
+				.then(resp -> {
+					switch resp {
+						case Some(v):
+							databasePassword = v.password;
+							resolve(databasePassword);
+						case None:
+							reject('No password provided');
+					}
+				});
+		});
+	}
+
+	function loadEntriesFromCSV():js.lib.Promise<Bool> {
+		return new Promise<Bool>((resolve, reject) -> {
+			if (dynamicDir != null) {
+				resolve(true);
+				return;
+			}
+
+			var args = [];
+
+			// export
+			args.push('export');
+			args.push('-q');
+			args.push('-f');
+			args.push('csv');
+			args.push(props.database_path);
+			var cp = js.node.ChildProcess.spawn('keepassxc-cli', args, {shell: true});
+
+			var data = '';
+			var error = '';
+
+			getKeePassXCPassword().then(password -> {
+				cp.stdin.write(password + '\n');
+
+				cp.stdout.on('data', d -> data += d);
+				cp.stdout.on('end', d -> {
+					var lineBreakEreg = ~/\r?\n/g;
+					var cleanData = lineBreakEreg.replace(data, '');
+					if (cleanData.length == 0) {
+						reject(error);
+					} else {
+						var lines = lineBreakEreg.split(data);
+
+						lines.shift();
+						var groupIndex = 0;
+						var titleIndex = 1;
+						var usernameIndex = 2;
+						var passwordIndex = 3;
+						var notesIndex = 5;
+
+						var items:Array<DynamicDirItem> = [];
+
+						inline function removeQuotes(s:String) {
+							if (s == null || s.length == 0)
+								return '';
+							return s.substring(1, s.length - 1);
+						}
+
+						var separatorText = 'separator:';
+						var delayText = 'delay:';
+						var tokens, group, title, username, password, notes, entryName;
+						var separator = '';
+						var delay = 0;
+						for (l in lines) {
+							separator = 'tab';
+							delay = 0;
+							tokens = l.split(',');
+
+							group = removeQuotes(tokens[groupIndex]);
+							if (group == '' || group.toLowerCase().startsWith('recycle'))
+								continue;
+							group = group.substring(group.indexOf('/') + 1, group.length);
+
+							if (props.database_root_folder != '' && !group.startsWith(props.database_root_folder))
+								continue;
+
+							title = removeQuotes(tokens[titleIndex]);
+							username = removeQuotes(tokens[usernameIndex]);
+							password = removeQuotes(tokens[passwordIndex]);
+							notes = removeQuotes(tokens[notesIndex]);
+							if (notes != '') {
+								for (n in notes.split(';')) {
+									if (n.startsWith(separatorText))
+										separator = n.replace(separatorText, '');
+									if (n.startsWith(delayText))
+										delay = Std.parseInt(n.replace(delayText, ''));
+								}
+							}
+
+							entryName = '$group/$title';
+							items.push({
+								text: entryName,
+								actions: [
+									{
+										name: 'log-in',
+										props: {
+											username: username,
+											password: password,
+											key_after_user: separator,
+											user_pass_delay: delay
+										}
+									}
+								]
+							});
+						}
+
+						if (props.parent_dir_state != null)
+							items.unshift(cast props.parent_dir_state);
+
+						var rows = 2;
+						var columns = 2;
+						while (rows * columns < items.length) {
+							rows++;
+							if (rows * columns >= items.length)
+								break;
+							columns++;
+						}
+						dynamicDir = {
+							rows: rows,
+							columns: columns,
+							items: items
+						}
+						resolve(true);
+					}
+				});
+				cp.stderr.on('data', e -> error += e);
+				cp.stderr.on('end', e -> {
+					if (error != '')
+						reject('Error in keepassxc-cli execution: $error');
+				});
+
+				cp.on('error', (error) -> {
+					reject('Error calling keepassxc-cli: $error');
+				});
+			}).catchError(error -> reject('Error getting keepassxc password: $error'));
+		});
+	}
+}
---
+++ readme.md
@@ -0,0 +1,52 @@
+# Action for [ideckia](https://ideckia.github.io/): keepassxc
+
+## Description
+
+Load all the password saved in the keepassxc database and creates an item for each
+
+## Properties
+
+| Name | Type | Description | Shared | Default | Possible values |
+| ----- |----- | ----- | ----- | ----- | ----- |
+| database_path | String | The path to the database | false | null | null |
+| database_root_folder | String | Show entries from this folder (empty shows all entries) | false | "" | null |
+| parent_dir_state | { toDir : String, textSize : UInt, textPosition : api.TextPosition, textColor : String, text : String, icon : String, bgColor : String } | Name of the parent directory | false | { toDir : "_main_", text : "back", textSize : null, textColor : null, textPosition : null, icon : "folder", bgColor : "ffff0000" } | null |
+
+## On single click
+
+Get entries from [KeePassXC](https://keepassxc.org/) application and creates a directory with an item for each entry.
+
+Action ['action_log-in'](http://github.com/ideckia/action_log-in) is required.
+
+When the action is initialized, it will get the entry content and will keep it in memory. If you want to reload the entry (if you have updated it), do it with a long press.
+
+## On long press
+
+Reloads the items from the database (asks for the password on every long press)
+
+## Test the action
+
+There is a script called `test_action.js` to test the new action. Set the `props` variable in the script with the properties you want and run this command:
+
+```
+node test_action.js
+```
+
+## Example in layout file
+
+```json
+{
+    "text": "KeePassXC example",
+    "bgColor": "00ff00",
+    "actions": [
+        {
+            "name": "keepassxc",
+            "props": {
+                "database_path": "/home/ideckia/passwords.kdbx",
+                    "database_root_folder": "",
+                    "parent_dir_state": { "toDir" : "_main_", "text" : "back", "textSize" : null, "textColor" : null, "textPosition" : null, "icon" : "folder", "bgColor" : "ffff0000" }
+            }
+        }
+    ]
+}
+```
