5368f438083148cf8b4b265392bd8030d08bfbaa
josuigoa <josuigoa@gmail.com>
2026-09-04 11:07:19 +0200

    get data from XML format instead of CSV

--- KeePassXC.hx
+++ KeePassXC.hx
@@ -1,5 +1,7 @@
 package;
 
+import haxe.xml.Access;
+
 using api.IdeckiaApi;
 using StringTools;
 
@@ -26,7 +28,7 @@ class KeePassXC extends IdeckiaAction {
 	override function init(initialState:ItemState):js.lib.Promise<ItemState> {
 		assertTextSizeProps();
 		if (props.cache_passwords)
-			loadEntriesFromCSV(initialState.textSize).then(dynDir -> dynamicDir = dynDir).catchError(e -> core.log.error(e));
+			loadEntriesFromXml(initialState.textSize).then(dynDir -> dynamicDir = dynDir).catchError(e -> core.dialog.error(Loc.error_dialog_title.tr(), e));
 		return super.init(initialState);
 	}
 
@@ -43,11 +45,11 @@ class KeePassXC extends IdeckiaAction {
 
 	public function execute(currentState:ItemState):js.lib.Promise<ActionOutcome> {
 		return new js.lib.Promise((resolve, reject) -> {
-			loadEntriesFromCSV(currentState.textSize).then(dynDir -> {
+			loadEntriesFromXml(currentState.textSize).then(dynDir -> {
 				if (props.cache_passwords)
 					dynamicDir = dynDir;
 				resolve(new ActionOutcome({directory: dynDir}));
-			}).catchError(e -> core.log.error(e));
+			}).catchError(e -> core.dialog.error(Loc.error_dialog_title.tr(), e));
 		});
 	}
 
@@ -71,13 +73,13 @@ class KeePassXC extends IdeckiaAction {
 							databasePassword = v.password;
 						resolve(v.password);
 					case None:
-						reject('No password provided');
+						reject(Loc.no_password_provided.tr());
 				}
 			});
 		});
 	}
 
-	function loadEntriesFromCSV(textSize:UInt):js.lib.Promise<DynamicDir> {
+	function loadEntriesFromXml(textSize:UInt):js.lib.Promise<DynamicDir> {
 		return new Promise<DynamicDir>((resolve, reject) -> {
 			if (dynamicDir != null) {
 				resolve(dynamicDir);
@@ -89,8 +91,6 @@ class KeePassXC extends IdeckiaAction {
 			// export
 			args.push('export');
 			args.push('-q');
-			args.push('-f');
-			args.push('csv');
 			args.push(props.database_path);
 			var cp = js.node.ChildProcess.spawn('keepassxc-cli', args, {shell: true});
 
@@ -105,75 +105,9 @@ class KeePassXC extends IdeckiaAction {
 					var lineBreakEreg = ~/\r?\n/g;
 					var cleanData = lineBreakEreg.replace(data, '');
 					if (cleanData.length == 0) {
-						reject(error);
+						reject(Loc.error_reading_db_wrong_pass.tr([props.database_path]));
 					} else {
-						var lines = lineBreakEreg.split(data);
-
-						lines.shift();
-						var groupIndex = 0;
-						var titleIndex = 1;
-						var usernameIndex = 2;
-						var passwordIndex = 3;
-						var notesIndex = 5;
-
-						var items:Array<DynamicDirItem> = [];
-
-						inline function removeQuotes(s:String) {
-							if (s == null || s.length == 0)
-								return '';
-							return s.substring(1, s.length - 1);
-						}
-
-						var separatorText = 'separator:';
-						var delayText = 'delay:';
-						var tokens, group, title, username, password, notes;
-						var separator = '';
-						var delay = 0;
-						for (l in lines) {
-							separator = 'tab';
-							delay = 0;
-							tokens = l.split(',');
-
-							group = removeQuotes(tokens[groupIndex]);
-							if (group == '' || group.toLowerCase().startsWith('recycle'))
-								continue;
-							group = group.substring(group.indexOf('/') + 1, group.length);
-
-							if (props.database_root_folder != '' && !group.startsWith(props.database_root_folder))
-								continue;
-
-							title = removeQuotes(tokens[titleIndex]);
-							if (title.startsWith('__'))
-								continue;
-							username = removeQuotes(tokens[usernameIndex]);
-							password = removeQuotes(tokens[passwordIndex]);
-							notes = removeQuotes(tokens[notesIndex]);
-							if (notes != '') {
-								for (n in notes.split(';')) {
-									if (n.startsWith(separatorText))
-										separator = n.replace(separatorText, '');
-									if (n.startsWith(delayText))
-										delay = Std.parseInt(n.replace(delayText, ''));
-								}
-							}
-
-							group = new RichString('$group/ ').size(textSize * (props.group_text_size_percent / 100));
-							title = new RichString(title).size(textSize * (props.title_text_size_percent / 100)).bold();
-							items.push({
-								text: '$group$title',
-								actions: [
-									{
-										name: 'log-in',
-										props: {
-											username: username,
-											password: password,
-											key_after_user: separator,
-											user_pass_delay: delay
-										}
-									}
-								]
-							});
-						}
+						var items = parseXml(data, textSize);
 
 						var rows = 2;
 						var columns = 2;
@@ -193,13 +127,99 @@ class KeePassXC extends IdeckiaAction {
 				cp.stderr.on('data', e -> error += e);
 				cp.stderr.on('end', e -> {
 					if (error != '')
-						reject('Error in keepassxc-cli execution: $error');
+						reject(Loc.error_cli_execution.tr(error));
 				});
 
 				cp.on('error', (error) -> {
-					reject('Error calling keepassxc-cli: $error');
+					reject(Loc.error_calling_cli.tr(error));
 				});
-			}).catchError(error -> reject('Error getting keepassxc password: $error'));
+			}).catchError(error -> reject(error));
 		});
+	}
+
+	function parseXml(data:String, textSize:UInt) {
+		var xml = Xml.parse(data);
+		var access = new Access(xml.firstElement());
+		var items = [];
+		parseElement(textSize, access.node.Root, '', items);
+		return items;
+	}
+
+	function parseElement(textSize:UInt, access:Access, group:String, items:Array<DynamicDirItem>) {
+		for (e in access.elements) {
+			switch e.name {
+				case 'Group':
+					var groupName = e.node.Name.innerData;
+					if (groupName == '' || groupName.toLowerCase().startsWith('recycle'))
+						continue;
+					if (props.database_root_folder != '' && !groupName.startsWith(props.database_root_folder))
+						continue;
+
+					groupName = groupName == 'Main' ? '' : groupName;
+					var newGroup = group == '' ? groupName : '$group/$groupName';
+					parseElement(textSize, e, newGroup, items);
+				case 'Entry':
+					var separatorText = 'separator:';
+					var delayText = 'delay:';
+					var title = '', username = '', password = '', notes;
+					var ignore = false;
+					var separator = '';
+					var delay = 0;
+					for (s in e.nodes.String) {
+						var key = getInnerData(s.node.Key);
+						var value = getInnerData(s.node.Value);
+
+						if (value.startsWith('__')) {
+							ignore = true;
+							break;
+						}
+						switch key.toLowerCase() {
+							case 'title':
+								title = value;
+							case 'username':
+								username = value;
+							case 'password':
+								password = value;
+							case 'notes':
+								notes = value;
+								if (notes != '') {
+									for (n in notes.split(';')) {
+										if (n.startsWith(separatorText))
+											separator = n.replace(separatorText, '');
+										if (n.startsWith(delayText))
+											delay = Std.parseInt(n.replace(delayText, ''));
+									}
+								}
+						}
+					}
+
+					if (!ignore) {
+						var richGroup = new RichString('$group/ ').size(textSize * (props.group_text_size_percent / 100));
+						var richTitle = new RichString(title).size(textSize * (props.title_text_size_percent / 100)).bold();
+						items.push({
+							text: '$richGroup$richTitle',
+							actions: [
+								{
+									name: 'log-in',
+									props: {
+										username: username,
+										password: password,
+										key_after_user: separator,
+										user_pass_delay: delay
+									}
+								}
+							]
+						});
+					}
+			}
+		}
+	}
+
+	function getInnerData(a:Access) {
+		try {
+			return a.innerData;
+		} catch (_) {
+			return '';
+		}
 	}
 }
